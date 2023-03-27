6b2a6e28e5b3e58d9209aae54ee849654e89d37a
Nguyễn Trọng Phúc <87481608+Horigamisan@users.noreply.github.com>
2023-03-27 08:24:30 +0700

    Add admin manage

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,22 +16,23 @@ namespace WebDemo
             routes.MapRoute("Product", "{type}/{meta}",
                 new { controller = "Product", action = "Index", meta = UrlParameter.Optional },
                 new RouteValueDictionary(new { type = "san-pham" }),
-                new[] { "WebDemo.Controllers" });
+                namespaces: new[] { "WebDemo.Controllers" });
 
             routes.MapRoute("Detail", "{type}/{meta}/{id}",
                new { controller = "Product", action = "getDetailProduct", meta = UrlParameter.Optional },
                new RouteValueDictionary(new { type = "san-pham" }),
-               new[] { "WebDemo.Controllers" });
+               namespaces: new[] { "WebDemo.Controllers" });
 
             routes.MapRoute("Contact", "{type}/{meta}",
                new { controller = "Contact", action = "Index", meta = UrlParameter.Optional },
                new RouteValueDictionary(new { type = "lien-he" }),
-               new[] { "WebDemo.Controllers" });
+               namespaces: new[] { "WebDemo.Controllers" });
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Default", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebDemo.Controllers" }
             );
         }
     }
---
+++ Areas/admin/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDemo.Areas.admin.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: admin/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
